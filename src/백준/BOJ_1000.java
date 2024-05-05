bbd7cdc21c6c74b84ae51f9af452477f240742ff
gisungPark <gisung2215@naver.com>
2024-05-05 18:27:43 +0900

    SOL_1000

---
+++ src/백준/BOJ_1000.java
@@ -0,0 +1,17 @@
+package 백준;
+
+import java.util.Scanner;
+
+public class BOJ_1000 {
+    public static void main(String[] args) {
+        int A, B;
+        Scanner scanner = new Scanner(System.in);
+        A = scanner.nextInt();
+        B = scanner.nextInt();
+        //scanner.next();
+        //scanner.nextDouble();
+
+        int result = A + B;
+        System.out.println(result);
+    }
+}
