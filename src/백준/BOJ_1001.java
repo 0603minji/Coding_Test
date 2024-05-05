c3900e9f88568d18d840c02f51ea627cc3f8efbe
gisungPark <gisung2215@naver.com>
2024-05-05 18:45:30 +0900

    SOL_1001

---
+++ src/백준/BOJ_1001.java
@@ -0,0 +1,17 @@
+package 백준;
+
+import java.util.Scanner;
+
+public class BOJ_1001 {
+    public static void main(String[] args) {
+        int A, B;
+        Scanner scanner = new Scanner(System.in);
+        A = scanner.nextInt();
+        B = scanner.nextInt();
+        //scanner.next();
+        //scanner.nextDouble();
+        int res = A - B;
+        System.out.println(res);
+    }
+}
+
